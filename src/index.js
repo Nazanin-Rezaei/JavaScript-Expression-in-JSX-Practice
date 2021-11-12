f94ca55c6d7c7751677142cf2870478b595560c7
nazaninrezaei88@gmail.com <nazaninrezaei88@gmail.com>
2021-11-12 11:08:29 -0500

    practiceg JavaScript expression in React

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fullName = "Nazanin Rezaei";
+const year = new Date();
+const currentYear = year.getFullYear();
+ReactDOM.render(
+  <div>
+    <p>Created by {fullName}.</p>
+    <p>Copyright {currentYear}</p>
+  </div>,
+  document.getElementById("root")
+);
